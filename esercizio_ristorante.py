7d4b5058d56bd724b0bd28c8630b8295a8d7ca80
Giacinto Guarino <60277809+jok8@users.noreply.github.com>
2024-04-05 14:07:05 +0200

    Correzione del codice

--- esercizio_ristorante.py
+++ esercizio_ristorante.py
@@ -1,52 +1,52 @@
 tavoli = False
-clienti = [1,2,3]
+tavoli_occupati = 0
 base = ["pan di spagna", "pan di zenzero", "pan au ciocolat"]
 listino_base = [2,5,7]
 farcitura = ["cioccolato", "marmellata di amarene", "marmellata di fragole"]
 listino_farcitura = [10, 12, 15]
 topping = ["cioccolato", "marmellata", "sciroppo d'acero"]
 listino_topping = [5, 7, 10]
-guadagni_base = False
-guadagni_farcitura = False
-guadagni_topping = False
+guadagni_base = [0,0,0]
+guadagni_farcitura = [0,0,0]
+guadagni_topping = [0,0,0]
 guadagno_torta_tavolo = [0,0,0]
-guadagno_totale = False
+guadagno_totale = [0,0,0]
 
 
 while tavoli == False:
     apertura = input("ristorante chiuso, scrivi apri per aprire\n")
     if apertura == "apri":
-        tavoli = True
+        tavoli = [1,2,3]
 
-while tavoli == True:
-    nome= input("benvenuti, mi dia un nome")
-    for i in clienti:
-        if i==0 or 1 or 2 or 3 or 4 or 5 or 6 or 7 or 8 or 9:
-            print(f"perfetto, il vostro tavolo è il {i}")
-            clienti[i]==nome
-        scelta_base= input("scegliete un ingrediente per la base")
+while tavoli == [1,2,3]:
+    for i in tavoli:
+        nome= input("benvenuti, mi dia un nome")
+        print(f"perfetto, il vostro tavolo è il {i}")
+        tavoli_occupati +=1
+        scelta_base= int(input("scegliete un ingrediente per la base: 1 per pan di spagna, 2 per pan di zenzero, 3 per pan au ciocolat"))
         if scelta_base == 1:
-            guadagni_base[scelta_base-1]= listino_base[scelta_base-1]
+            guadagni_base[0]= listino_base[0]
         elif scelta_base == 2:
-            guadagni_base[scelta_base-1]= listino_base[scelta_base-1]
+            guadagni_base[1]= listino_base[1]
         elif scelta_base == 3:
-            guadagni_base[scelta_base-1]= listino_base[scelta_base-1]
-        scelta_farcitura = input("scegliete la farcitura")
+            guadagni_base[2]= listino_base[2]
+        scelta_farcitura = int(input("scegliete la farcitura: 1 per cioccolato, 2 per marmellata di amarene, 3 per marmellata di fragole"))
         if scelta_farcitura ==1:
-            guadagni_farcitura[scelta_farcitura-1] = listino_farcitura[scelta_farcitura-1]
+            guadagni_farcitura[0] = listino_farcitura[0]
         elif scelta_farcitura ==2:
-            guadagni_farcitura[scelta_farcitura-1] = listino_farcitura[scelta_farcitura-1]
+            guadagni_farcitura[1] = listino_farcitura[1]
         elif scelta_farcitura ==3:
-            guadagni_farcitura[scelta_farcitura-1] = listino_farcitura[scelta_farcitura-1]
-        scelta_topping = input("scegliete il topping")
+            guadagni_farcitura[2] = listino_farcitura[2]
+        scelta_topping = int(input("scegliete il topping: 1 per cioccolato, 2 per marmellata, 3 per sciroppo d'acero"))
         if scelta_topping ==1:
-            guadagni_topping[scelta_topping-1] = listino_topping[scelta_topping-1]
+            guadagni_topping[0] = listino_topping[0]
         elif scelta_topping ==2:
-            guadagni_topping[scelta_topping-1] = listino_topping[scelta_topping-1]
+            guadagni_topping[1] = listino_topping[1]
         elif scelta_topping ==3:
-            guadagni_topping[scelta_topping-1] = listino_topping[scelta_topping-1]
-        for i in guadagno_torta_tavolo:
-            guadagno_torta_tavolo[i] = guadagni_base[i]+guadagni_farcitura[i]+guadagni_topping[i]
-            print(f"Il prezzo della vostra torta è {guadagno_torta_tavolo[i]} euro")
+            guadagni_topping[2] = listino_topping[2]
+        guadagno_torta_tavolo[i-1] = guadagni_base[scelta_base-1]+guadagni_farcitura[scelta_farcitura-1]+guadagni_topping[scelta_topping-1]
+        print(f"Il prezzo della vostra torta è {guadagno_torta_tavolo[i-1]} euro")
+        if tavoli_occupati == 3:
+            break
     guadagno_totale= sum(guadagno_torta_tavolo)
     print(f"Il guadagno totale è {guadagno_totale} euro")
