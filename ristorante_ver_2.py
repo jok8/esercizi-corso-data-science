b06e26796567027f751cb57b02b399a0a1294b40
Giacinto Guarino <60277809+jok8@users.noreply.github.com>
2024-04-08 14:51:08 +0200

    caricato il piano per la versione 2 del ristorante

---
+++ ristorante_ver_2.py
@@ -0,0 +1,10 @@
+#creo una classe chiamata ristorante con un valore descrizione uguale a 0 collegato ad un successivo metodo "aggiungi descrizione"
+#un dizionario vuoto che rappresenta il menu
+#definisco un metodo init con nome e tipo di cucina e il parametro aperto=False
+
+#metodi della classe:
+#aggiungi_descrizione, che chiede input di descrivere il ristorante e imposta il contatore a 1
+#descrivi_ristorante dove if contatore = 0 printa "devi prima aggiungere una descrizione", se è uguale a 1, stampa aggiungi descrizione
+#stato_apertura stampa semplicemente il valore del parametro apertura
+#apri_ristorante e chiudi_ristorante
+#aggiungi_al_menu dove imposto un valore
