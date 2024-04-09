dceb87abfb575272c5cae602383cd33c78cff64d
Giacinto Guarino <60277809+jok8@users.noreply.github.com>
2024-04-09 15:56:57 +0200

    caricato il piano dell'esercizio sulla libreria

---
+++ esercizio_libreria.py
@@ -0,0 +1,6 @@
+#faccio una classe madre libreria con attributo catalogo
+    #metodi aggiungi libro, rimuovi libro, cerca per titolo, mostra catalogo
+
+#faccio una classe figlia libro che accetta attributi titolo, autore e isbn
+    #metodi descrizione, che descrive il libro usando tutti e tre gli attributi
+
