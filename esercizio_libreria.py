f199c91f43f28526529b7668d983503098e119c7
Giacinto Guarino <60277809+jok8@users.noreply.github.com>
2024-04-09 17:11:20 +0200

    Update esercizio_libreria.py

--- esercizio_libreria.py
+++ esercizio_libreria.py
@@ -4,3 +4,48 @@
 #faccio una classe figlia libro che accetta attributi titolo, autore e isbn
     #metodi descrizione, che descrive il libro usando tutti e tre gli attributi
 
+
+
+
+class Libro:
+    def __init__ (self, titolo, autore, ISBN):
+        self.titolo = titolo
+        self.autore = autore
+        self.ISBN = ISBN
+    
+
+class Libreria(Libro):
+    catalogo = {}
+    def __init__ (self, titolo, autore, ISBN):
+        super().__init__(titolo, autore, ISBN)
+    
+    def aggiungi_libro(self, titolo, autore, ISBN):
+        super().__init__(titolo, autore, ISBN)
+        if ISBN in Libreria.catalogo:
+            print("Libro già presente nel catalogo")
+        else:
+            Libreria.catalogo[titolo] = {"Autore": autore, "ISBN": ISBN}
+
+    def rimuovi_libro(self, titolo, ISBN):
+        del Libreria.catalogo[titolo][ISBN]
+    
+    def cerca_libro(self,Libro):
+        if Libro in Libreria.catalogo:
+            print(f"{Libro} è presente nel catalogo")
+    
+    def mostra_catalogo(self):
+        print(Libreria.catalogo)
+
+
+while True:
+    aggiunta_libro = input("vuoi aggiungere un libro?")
+    if aggiunta_libro == "sì":
+        nome_libro = input("Aggiungi un nome")
+        autore_libro = input("Aggiungi l'autore")
+        ISBN_libro = input("Aggiungi ISBN")
+        libro_aggiunto = Libreria(nome_libro, autore_libro, ISBN_libro)
+        libro_aggiunto.aggiungi_libro(nome_libro, autore_libro, ISBN_libro)
+    else:
+        print("ok, allora non aggiungiamo nulla")
+        input("cosa vuoi fare?")
+        
