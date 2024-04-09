abf625597c558e27852660c1ddc0cd5a8313a4ce
Giacinto Guarino <60277809+jok8@users.noreply.github.com>
2024-04-09 17:31:01 +0200

    Update esercizio_libreria.py

--- esercizio_libreria.py
+++ esercizio_libreria.py
@@ -29,23 +29,38 @@ class Libreria(Libro):
     def rimuovi_libro(self, titolo, ISBN):
         del Libreria.catalogo[titolo][ISBN]
     
-    def cerca_libro(self,Libro):
+    def cerca_libro(Libro):
         if Libro in Libreria.catalogo:
             print(f"{Libro} è presente nel catalogo")
     
-    def mostra_catalogo(self):
+    def mostra_catalogo():
         print(Libreria.catalogo)
 
 
 while True:
-    aggiunta_libro = input("vuoi aggiungere un libro?")
+    aggiunta_libro = input("vuoi aggiungere un libro?\n")
     if aggiunta_libro == "sì":
-        nome_libro = input("Aggiungi un nome")
-        autore_libro = input("Aggiungi l'autore")
-        ISBN_libro = input("Aggiungi ISBN")
+        nome_libro = input("Aggiungi un nome\n")
+        autore_libro = input("Aggiungi l'autore\n")
+        ISBN_libro = input("Aggiungi ISBN\n")
         libro_aggiunto = Libreria(nome_libro, autore_libro, ISBN_libro)
         libro_aggiunto.aggiungi_libro(nome_libro, autore_libro, ISBN_libro)
     else:
         print("ok, allora non aggiungiamo nulla")
-        input("cosa vuoi fare?")
-        
+        cosa_fare = int(input("cosa vuoi fare?\nScrivi:\n0 per rimuovere un libro\n1 per cercare un libro\n2 per mostrare il catalogo\n3 per uscire\n"))
+        if cosa_fare == 0:
+            libro_da_rimuovere = input("che libro vuoi rimuovere?\n")
+            Libreria.rimuovi_libro(libro_da_rimuovere)
+        elif cosa_fare == 1:
+            libro_da_cercare = input("che libro vuoi cercare?\n")
+            Libreria.cerca_libro(libro_da_cercare)
+        elif cosa_fare == 2:
+            print("Il catalogo è questo")
+            Libreria.mostra_catalogo()
+        elif cosa_fare == 3:
+            print("uscita selezionata, arrivederci")
+            break
+        else:
+            print("nessuna azione selezionata, arrivederci")
+            break
+
