f595627901587da0a9794f602b7f4c0b1ef8cd0d
Giacinto Guarino <60277809+jok8@users.noreply.github.com>
2024-04-10 10:54:35 +0200

    Add files via upload

---
+++ esercizio_analisi_vendite.py
@@ -0,0 +1,11 @@
+# creo la classe padre registro vendite
+#     creo un dizionario che verrà compilato dall'oggetto della classe figlia
+# creo la classe figlia giorno di vendita con 1, 2, 3, 30
+#     creo un metodo che aggiunga i dati al dizionario dove chiede se vuole inserire i valori con un while
+#         creo un try e except all'interno per gestire valueerror quando:
+#                             - l'utente inserisce una lettera al posto di un numero/ il numero non è int
+#                             - il numero è minore di 0
+#     creo un metodo che calcola la somma con un for loop
+#     creo un metodo che calcoli la media basandosi sul metodo della somma
+#     creo un metodo che trovi in dictionary tutte le vendite al di sopra della media con un for loop
+
